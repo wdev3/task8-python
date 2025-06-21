a02e0174611cfbb5b27c40303cc9a98b0c491e48
wdev <wendellcesar1245@gmail.com>
2025-06-21 14:43:58 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+Carro = {
+    "brand": "Toyota", # Changed 'marca' to 'brand'
+    "model": "Corolla", # Changed 'Modelo' to 'model'
+    "year": 2020,     # Changed 'Ano' to 'year'
+    "color": "Silver" # Changed 'cor' to 'color' and 'Prata' to 'Silver'
+}
